07cb59b2dff500cfa66966844d203ad260dedf9e
Max Mattiasson <max@mattiasson.eu>
2026-04-01 13:47:02 +0200

    added tsconfig to backend, mongoose connected DB

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("funking tjoho");
