cebd1591a5479fb6b5a9b8c535994da71b07cebc
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-04-06 23:19:31 +0200

    get, post och delete

--- apps/backend/src/routes/gamesRoutes.ts
+++ apps/backend/src/routes/gamesRoutes.ts
@@ -3,29 +3,70 @@ import Game from "../models/Game.js";
 
 const router = Router();
 
+// lista spel med title, genre, created, dev och multiplayer
 router.get("/games", async (req, res) => {
-  const games = await Game.find();
-
-  res.json(games);
-});
-
-// sök efter spel med title
-router.get("/games/title/:title", async (req, res) => {
+  console.log("funkar?");
   try {
-    const { title } = req.params;
-    // hitta titlar som matchar title sökt, case-insensitive
-    const game = await Game.findOne({
-      title: { $regex: title, $options: "i" },
-    });
+    const { title, genre, created, dev, multiplayer } = req.query as {
+      title?: string;
+      genre?: string;
+      created?: string;
+      dev?: string;
+      multiplayer?: "true" | "false";
+    };
+    const filter: any = {};
 
-    if (!game) {
-      return res
-        .status(404)
-        .json({ message: "Inget spel med den titeln hittades." });
-    }
-    res.json(game);
+    // matchar sökt, case-insensitive
+    if (title) filter.title = { $regex: title, $options: "i" };
+    if (genre) filter.genre = { $regex: genre, $options: "i" };
+    if (created) filter.created = created;
+    if (dev) filter.dev = { $regex: dev, $options: "i" };
+    if (multiplayer !== undefined) filter.multiplayer = multiplayer === "true";
+
+    const games = await Game.find(filter);
+    res.json(games);
   } catch (error) {
     res.status(500).json({ message: "Fel vid hämntning av spel", error });
+  }
+});
+
+// Lägg till spel
+router.post("/", async (req, res) => {
+  console.log("funkar?");
+
+  const game = new Game({
+    title: req.body.title,
+    created: req.body.created,
+    dev: req.body.dev,
+    genre: req.body.genre,
+    multiplayer: req.body.multiplayer
+  });
+  try {
+    const newGame = await game.save();
+    res.status(201).json(newGame);
+  } catch (error) {
+    res.status(400).json({ message: "De gick inte" });
+  }
+});
+
+// Radera spel via id
+router.delete("/games/:id", async (req, res) => {
+  console.log("funkar?");
+
+  try {
+    const deletedGame = await Game.findByIdAndDelete(req.params.id);
+
+    if (!deletedGame) {
+      return res.status(404).json({ message: "Inget spel hittades" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Spelet togs bort", game: deletedGame });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Fel vid borttagning av spel", error });
   }
 });
 
