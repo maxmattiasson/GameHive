42cd0e42b1338ba397da2f97f23e62ba8a8f061a
Baimako <sascha.bergenas@hotmail.com>
2026-04-06 19:36:53 +0200

    crud

--- apps/backend/src/routes/gamesRoutes.ts
+++ apps/backend/src/routes/gamesRoutes.ts
@@ -9,4 +9,24 @@ router.get("/games", async (req, res) => {
   res.json(games);
 });
 
+// sök efter spel med title
+router.get("/games/title/:title", async (req, res) => {
+  try {
+    const { title } = req.params;
+    // hitta titlar som matchar title sökt, case-insensitive
+    const game = await Game.findOne({
+      title: { $regex: title, $options: "i" },
+    });
+
+    if (!game) {
+      return res
+        .status(404)
+        .json({ message: "Inget spel med den titeln hittades." });
+    }
+    res.json(game);
+  } catch (error) {
+    res.status(500).json({ message: "Fel vid hämntning av spel", error });
+  }
+});
+
 export default router;
