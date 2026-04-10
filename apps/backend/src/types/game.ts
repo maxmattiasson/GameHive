b648ef3ad110e3ee1a9562fd287f2eec756046b8
Isabelle Franzen <isabelle.amsalu@gmail.com>
2026-04-10 11:28:11 +0200

    feat(seed/games): add game types

---
+++ apps/backend/src/types/game.ts
@@ -0,0 +1,12 @@
+export type Game = {
+  title: string;
+  release: string;
+  dev: string;
+  genres: string[];
+  platforms: string[];
+  desc: string;
+  thumb: string;
+  multiplayer: boolean;
+  avg_rating: number;
+  review: any[];
+};
