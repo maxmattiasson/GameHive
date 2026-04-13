f679f7b0b4f9befb6271ff8b5171e01f9b7c52fb
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-04-13 14:35:59 +0200

    style(gameCard): update card styling and add arrow button css

--- apps/backend/src/models/Game.ts
+++ apps/backend/src/models/Game.ts
@@ -46,7 +46,7 @@ const gameSchema = new mongoose.Schema(
       type: [mongoose.Schema.Types.Mixed], // change later when review type exists
     },
   },
-  { collection: "Games" },
+  { collection: "games" },
 );
 
 const Game = mongoose.model("Game", gameSchema);
