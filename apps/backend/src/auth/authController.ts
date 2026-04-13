2bf1484cf176fae2b0904355090307d920ba2d72
Max Mattiasson <max@mattiasson.eu>
2026-04-13 10:38:42 +0200

    jwt check added a protected route for /me

--- apps/backend/src/auth/authController.ts
+++ apps/backend/src/auth/authController.ts
@@ -81,7 +81,7 @@ export const login = async (req: Request, res: Response) => {
 
     res.status(200).json({
         message: "Login successful",
-        token,
+        token,  
         user: {
             id: user._id,
             username: user.username,
--- apps/backend/src/auth/authRoutes.ts
+++ apps/backend/src/auth/authRoutes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { login, signup } from "./authController.js" 
+import { authMiddleware, AuthRequest } from "./authMiddleware.js";
  
 const router = Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
+
+router.get("/me", authMiddleware, (req: AuthRequest, res) => {
+    res.status(200).json({
+      message: "Authenticated",
+      user: req.user,
+    });
+  });
 
 export default router;
 
