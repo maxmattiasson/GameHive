3c1cd3f667676bb4151c348e6ddec516ba501d60
Max Mattiasson <max@mattiasson.eu>
2026-04-13 10:43:11 +0200

    find actual person

--- apps/backend/src/auth/authRoutes.ts
+++ apps/backend/src/auth/authRoutes.ts
@@ -1,46 +1,35 @@
 import { Router } from "express";
 import { login, signup } from "./authController.js" 
 import { authMiddleware, AuthRequest } from "./authMiddleware.js";
+import UserModel from "../models/User.js";
  
 const router = Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
 
-router.get("/me", authMiddleware, (req: AuthRequest, res) => {
-    res.status(200).json({
-      message: "Authenticated",
-      user: req.user,
+router.get("/me", authMiddleware, async (req: AuthRequest, res) => {
+    try {
+        const userId = req.user?.userId;
+    
+        if (!userId) {
+          res.status(401).json({ message: "Unauthorized" });
+          return;
+        }
+    
+        const user = await UserModel.findById(userId).select("-passwordHash");
+    
+        if (!user) {
+          res.status(404).json({ message: "User not found" });
+          return;
+        }
+    
+        res.status(200).json(user);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error" });
+      }
     });
-  });
-
+    
 export default router;
 
-// router.get("/", async (req, res) => {
-//     try {
-//         const users = await UserModel.find()
-//         res.json(users)
-//     } catch (err) {
-//         console.error(err); 
-//         res.status(500).json({ message: "Server error"})
-//     }
-// })
-
-// router.get("/:id", async (req: Request, res: Response) => {
-//     try {
-//         const users = await UserModel.
-//         const user = users.find(u => u.id === parseInt(req.params.id))
-//     }
-    
-//     if (!user) {
-//         return res.status(404).json({message: "Finns ingen använderare", error: "user not found"};
-
-//         )
-//     }
-// })
-
-// router.post("/", (req, res) => {
-//     const newUser = { id: users.length + 1, ...req.body};
-//     users.push(newUser);
-//     res.status(201).json(newUser)
-// })
