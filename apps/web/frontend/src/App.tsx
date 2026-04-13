a8e135a8d0506688d071f99d7a4e8c0dedb30f98
Björn Boman <gostabohman@gmail.com>
2026-04-13 14:30:00 +0200

    Ser bedrövligt ut, men det får det göra nu

--- apps/web/frontend/src/App.tsx
+++ apps/web/frontend/src/App.tsx
@@ -4,26 +4,20 @@ import { GameList } from "./components/games/GameList"
 function App() {
   return (
     <>
-      // Header component here!
       <header>
-        // Logo
         <div>
           <h1>Game Hive</h1>
         </div>
-        // Nav
         <div>
           <span>Home</span>
           <span>Games</span>
           <span>About</span>
         </div>
-        // User menu (Login/Logout, avatar, screen name, menu button)
         <div>
           Sign up/Log in
         </div>
       </header>
-      // Move ^^this^^ into Header component, or not? 
       
-      // Page component here!
       <h1
         style={{
           display: "flex",
@@ -36,13 +30,11 @@ function App() {
       <div className="center">
         <GameList limit={3} />
       </div>
-      // Move ^^this^^ into Home(?) component // Footer component here!
       <footer>
-        // About: List of links to info articles 
+        { /* About: List of links to info articles */ }
         
-        // Contact: List of contact paths to the site developers
+        {/* Contact: List of contact paths to the site developers */}
       </footer>
-      // Move ^^this^^ into Footer component, or not?
     
     </>
   )
