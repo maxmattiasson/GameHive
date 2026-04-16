d606fd6a4d10febabe1d9b6e432f29bb515843f4
bjorneboman <gostabohman@gmail.com>
2026-04-17 00:45:09 +0200

    Pressing on!

--- apps/web/frontend/src/components/layout/Header.tsx
+++ apps/web/frontend/src/components/layout/Header.tsx
@@ -4,15 +4,15 @@ export function Header() {
     return(
         <>
             <header>
-                <div>
+                <div className="logo-container">
                     <h1>Game Hive</h1>
                 </div>
-                <div>
+                <div className="nav-container">
                     <span>Home</span>
                     <span>Games</span>
                     <span>About</span>
                 </div>
-                <div>
+                <div className="user-container">
                     Sign up/Log in
                 </div>
             </header>
