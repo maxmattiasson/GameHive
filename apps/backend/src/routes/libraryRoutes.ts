799f9e02133d8d1dda6cf5599bcc0004dccc3261
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-04-20 21:27:23 +0200

    a start on library router

---
+++ apps/backend/src/routes/libraryRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  getPlayerLibrary,
+  addToLibrary,
+  updateLibraryEntry
+} from "../controllers/libraryController.js";
+
+const router = Router();
+
+// list players library games
+router.get("/library", getPlayerLibrary);
+
+// add to players library
+router.post("/library", addToLibrary);
+
+// edit players game in library
+router.patch("/library/:id", updateLibraryEntry);
