bd1b4acbadab70a50ce5fad4ddd57bd9a2e3af6e
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-04-20 20:42:55 +0200

    return link

--- apps/web/frontend/src/pages/playerPage/PlayerAchivementsPage.tsx
+++ apps/web/frontend/src/pages/playerPage/PlayerAchivementsPage.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function PlayerAchivementsPage() {
   return (
     <section>
       <h1>Player Achivements</h1>
       <p>Your achivements here</p>
+      <Link to="/profile">Back to profile</Link>
     </section>
   );
 }
