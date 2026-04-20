72bc326560c7b774397b6db70fe2c0411d2a2e5a
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-04-20 10:27:44 +0200

    moved out logic on gamelist and limit

--- apps/web/frontend/src/components/games/GameList.tsx
+++ apps/web/frontend/src/components/games/GameList.tsx
@@ -1,20 +1,12 @@
-import { useGames } from "../../hooks/useGames";
+import type { Game } from "../../types/game";
 import { GameCard } from "./GameCard";
 import "./GameList.css";
 
 interface Props {
-  limit?: number;
+  games: Game[];
 }
 
-export function GameList({ limit }: Props) {
-  const { data, loading, error } = useGames();
-
-  //early returns
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error} </p>;
-
-  const games = limit ? data.slice(0, limit) : data; //slice array to limit, otherwise return all
-
+export function GameList({ games }: Props) {
   return (
     <div className="game-list">
       {games.map((game) => (
