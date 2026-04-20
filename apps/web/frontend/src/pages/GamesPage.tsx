438873087876cee1b7cae822278dde547b77a3c6
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-04-20 10:28:04 +0200

    filter function on gamepage

--- apps/web/frontend/src/pages/GamesPage.tsx
+++ apps/web/frontend/src/pages/GamesPage.tsx
@@ -1,10 +1,57 @@
-export function GamesPage() {
-    return(
-        <>
-            <p>Hush! 🤫 You're in a library... </p>
-            <h1>OUR GAMES LIBRARY!! 🥳🎮🕹️👾</h1>
+import { useState } from "react";
+import GameFilter from "../components/games/gameFilter/GameFilter";
+import { GameList } from "../components/games/GameList";
+import type { GameFilterValues } from "../types/gameFilter";
+import { useGames } from "../hooks/useGames";
 
-            <p>(Tons and tons of games here....)</p>
-        </>
-    )
+const initialFilters: GameFilterValues = {
+  title: "",
+  genre: [],
+  dev: "",
+  release: "",
+  multiplayer: false
+};
+
+export function GamesPage() {
+  const [activeFilters, setActiveFilters] =
+    useState<GameFilterValues>(initialFilters);
+  const { data, loading, error } = useGames();
+  const filteredGames = data.filter((game) => {
+    const titleMatch = game.title
+      .toLowerCase()
+      .includes(activeFilters.title.toLowerCase().trim());
+
+    const devMatch = game.dev
+      .toLocaleLowerCase()
+      .includes(activeFilters.dev.toLocaleLowerCase().trim());
+
+    const releaseYear = new Date(game.release).getFullYear().toString();
+    const releaseMatch =
+      activeFilters?.release.trim() === "" ||
+      releaseYear.includes(activeFilters.release.trim());
+
+    const genreMatch =
+      activeFilters?.genre.length === 0 ||
+      activeFilters?.genre.some((selectedGenre) =>
+        game.genres.includes(selectedGenre)
+      );
+
+    const multiplayerMatch = !activeFilters?.multiplayer || game.multiplayer;
+
+    return (
+      titleMatch && devMatch && releaseMatch && genreMatch && multiplayerMatch
+    );
+  });
+  return (
+    <>
+      <p>Hush! 🤫 You're in a library... </p>
+      <h1>OUR GAMES LIBRARY!! 🥳🎮🕹️👾</h1>
+
+      <p>(Tons and tons of games here....)</p>
+      <div className="game-filter-wrapper">
+        <GameFilter onSearch={setActiveFilters} />
+        <GameList games={filteredGames} />
+      </div>
+    </>
+  );
 }
