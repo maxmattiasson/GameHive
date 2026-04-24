011c59c78cd51d5e5d733059d4ee056940f7f144
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-04-24 11:47:40 +0200

    feat(friendship): add post route

---
+++ apps/backend/src/routes/friendshipRoutes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { sendFriendRequest } from "../controllers/friendshipControllers.js";
+import { authMiddleware } from "../auth/authMiddleware.js";
+
+const router = Router();
+
+router.post("/requests", authMiddleware, sendFriendRequest);
