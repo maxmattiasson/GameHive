c2bb7d6fc5e6bc901438dd6f983593b082e15617
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-04-24 12:25:07 +0200

    feat(friendship) add GET for pending requests

--- apps/backend/src/routes/friendshipRoutes.ts
+++ apps/backend/src/routes/friendshipRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { sendFriendRequest } from "../controllers/friendshipControllers.js";
+import {
+  sendFriendRequest,
+  getPendingRequests,
+} from "../controllers/friendshipControllers.js";
 import { authMiddleware } from "../auth/authMiddleware.js";
 
 const router = Router();
 
 router.post("/requests", authMiddleware, sendFriendRequest);
+router.get("/requests", authMiddleware, getPendingRequests);
 
 export default router;
