8fc48173daa0f689fd54644325fd70b52723317e
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-05-06 09:06:15 +0200

    feat(profile): update headings and add placeholder profile picture

--- apps/web/frontend/src/pages/playerPage/PlayerProfilePage.tsx
+++ apps/web/frontend/src/pages/playerPage/PlayerProfilePage.tsx
@@ -1,22 +1,28 @@
 import { Link, Outlet } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import "./PlayerProfilePage.css";
 
 export function PlayerProfile() {
   const { user } = useAuth();
   return (
-    <div>
+    <div className="player-profile">
       <h1>Player Profile</h1>
       <p>Welcome {user?.username}</p>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJ09O7DcXW62RYeG11IAOVukc5tNBerllXA&s" />
+      </div>
       <br />
-      <p>
-        <Link to="/profile/friends">Friends</Link>
-      </p>
-      <p>
-        <Link to="/profile/achivements">Achivements</Link>
-      </p>
-      <p>
-        <Link to="/profile/library">Library</Link>
-      </p>
+      <ul>
+        <li>
+          <Link to="/profile/friends">Friends</Link>
+        </li>
+        <li>
+          <Link to="/profile/achivements">Achivements</Link>
+        </li>
+        <li>
+          <Link to="/profile/library">Library</Link>
+        </li>
+      </ul>
       <Outlet />
     </div>
   );
