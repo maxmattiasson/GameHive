c8c967946b7c3d6f2b821a310cc387332bcc7a31
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-05-06 11:36:30 +0200

    feat(profile) fetch and display other players profile

--- apps/web/frontend/src/pages/playerPage/PlayerProfilePage.tsx
+++ apps/web/frontend/src/pages/playerPage/PlayerProfilePage.tsx
@@ -1,16 +1,27 @@
 import { Outlet, NavLink, useParams } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useState, useEffect } from "react";
 import "./PlayerProfilePage.css";
 
 export function PlayerProfile() {
   const { user } = useAuth();
   const { id } = useParams();
-  console.log(id);
+
+  const [otherUser, setOtherUser] = useState<any>(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    fetch(`/api/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setOtherUser(data));
+  }, [id]);
+
   return (
     <div className="player-profile section">
       <div className="player-header border-10">
         <div>
-          {id ? <h1>{id}</h1> : <h1>{user?.username}</h1>}
+          {id ? <h1>{otherUser?.username}</h1> : <h1>{user?.username}</h1>}
           <p>Player Profile</p>
         </div>
         <div className="player-img">
