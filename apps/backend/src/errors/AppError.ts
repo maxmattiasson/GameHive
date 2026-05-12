1d26bd03c4af78a984d8b0b86e348ce57c61815d
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-05-12 15:24:06 +0200

    change to ts and update exports

--- apps/backend/src/errors/AppError.ts
+++ apps/backend/src/errors/AppError.ts
@@ -1,7 +1,10 @@
 // Base class for all expeted application errors
 
 export class AppError extends Error {
-  constructor(message, statusCode) {
+  statusCode: number;
+  isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
     super(message);
     this.name = this.constructor.name;
     this.statusCode = statusCode;
@@ -13,7 +16,9 @@ export class AppError extends Error {
 // 400 - Validation error, faulty in-data
 
 export class ValidationError extends AppError {
-  constructor(message = "Validation Erorr", errors = []) {
+  errors: any[];
+
+  constructor(message: string = "Validation Erorr", errors: any[] = []) {
     super(message, 400);
     this.errors = errors;
   }
@@ -22,7 +27,12 @@ export class ValidationError extends AppError {
 // 401 - Authorization is required (useable in week 7)
 
 export class UnothorizedError extends AppError {
-  constructor(message = "Authorization is required", errors = []) {
+  errors: any[];
+
+  constructor(
+    message: string = "Authorization is required",
+    errors: any[] = [],
+  ) {
     super(message, 401);
     this.errors = errors;
   }
@@ -31,7 +41,7 @@ export class UnothorizedError extends AppError {
 // 403 - Access Denied (usable in week 7)
 
 export class ForbiddenError extends AppError {
-  constructor(message = "Access Denied") {
+  constructor(message: string = "Access Denied") {
     super(message, 403);
   }
 }
@@ -39,14 +49,14 @@ export class ForbiddenError extends AppError {
 // 404 - Not found
 
 export class NotFoundError extends AppError {
-  constructor(message = "The resource could not be located") {
+  constructor(message: string = "The resource could not be located") {
     super(message, 404);
   }
 }
 
 // 409 - Conflict Error
 export class ConflictError extends AppError {
-  constructor(message = "The resource is already present") {
+  constructor(message: string = "The resource is already present") {
     super(message, 409);
   }
 }
