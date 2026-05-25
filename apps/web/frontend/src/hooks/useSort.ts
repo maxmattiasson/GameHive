5661a8507a02ace49aa5583e7752ea7f18cbb98f
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-05-25 14:51:33 +0200

    null, no id and undefined filtering

--- apps/web/frontend/src/hooks/useSort.ts
+++ apps/web/frontend/src/hooks/useSort.ts
@@ -9,6 +9,12 @@ export function useSort<T>(
   // only sort if item, SortBy or order changes, item creates a copy of the original array
   return useMemo(() => {
     return [...items].sort((a, b) => {
+      if (!a && !b) return 0;
+      if (!a) return 1;
+      if (!b) return -1;
+      if (a[sortBy] == null && b[sortBy] == null) return 0;
+      if (a[sortBy] == null) return 1;
+      if (b[sortBy] == null) return -1;
       if (a[sortBy] < b[sortBy]) return order === "asc" ? -1 : 1;
       if (a[sortBy] > b[sortBy]) return order === "asc" ? 1 : -1;
       return 0;
