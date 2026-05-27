937ec886c21440d740c2302643adf49f95bd06b9
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-05-27 16:09:06 +0200

    update admin

--- apps/web/frontend/src/pages/adminPage/AdminPage.tsx
+++ apps/web/frontend/src/pages/adminPage/AdminPage.tsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import Button from "../../components/ui/Button";
+import type { User } from "../../types/user";
 
 const AdminPage = () => {
-  const [users, setUsers] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [filter, setFilter] = useState("");
 
+  const findUsers = async (id: string) => {
+    try {
+      const response = await fetch(
+        `/api/users?search=${encodeURIComponent(filter)}`,
+        {
+          credentials: "include"
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setUsers(data);
+      }
+    } catch (error) {
+      // hantera fel
+    }
+  };
   const deleteUser = async (id: string) => {
     try {
       const response = await fetch(`/api/users/${id}`, {
         method: "DELETE",
-        headers: {
-          "Content-Type": "application/json"
-        },
         credentials: "include"
       });
       if (response.ok) {
-        // ta bort användare och uppdatera state
-      } else {
-        //hantera fel
+        setUsers((user) => users.filter((user) => user._id !== id));
       }
     } catch (error) {
       // hantera fel
@@ -26,16 +39,10 @@ const AdminPage = () => {
   return (
     <div>
       <h1>ADMIN</h1>
-      <form action="">
-        <fieldset>
-          <legend>Delete user</legend>
-          <input placeholder="User Id" />
 
-          <Button type="submit" color="primary">
-            Delete
-          </Button>
-        </fieldset>
-      </form>
+      <fieldset>
+        <legend>Delete user</legend>
+      </fieldset>
     </div>
   );
 };
