e5005bdc7a4ff20434f02b0ef2d8e976e5deb152
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-05-27 14:03:53 +0200

    admin page layout

--- apps/web/frontend/src/pages/adminPage/AdminPage.tsx
+++ apps/web/frontend/src/pages/adminPage/AdminPage.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import Button from "../../components/ui/Button";
+
 const AdminPage = () => {
+  const [users, setUsers] = useState("");
+
+  const deleteUser = async (id: string) => {
+    try {
+      const response = await fetch(`/api/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+      if (response.ok) {
+        // ta bort användare och uppdatera state
+      } else {
+        //hantera fel
+      }
+    } catch (error) {
+      // hantera fel
+    }
+  };
+
   return (
     <div>
       <h1>ADMIN</h1>
-      <p>Find and delete them maggots!</p>
-      <input placeholder="Find user" />
+      <form action="">
+        <fieldset>
+          <legend>Delete user</legend>
+          <input placeholder="User Id" />
+
+          <Button type="submit" color="primary">
+            Delete
+          </Button>
+        </fieldset>
+      </form>
     </div>
   );
 };
