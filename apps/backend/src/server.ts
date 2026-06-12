3b7aea316ebd040410981983dfcf83100c988b72
Sascha Bergenås <sascha.bergenas@hotmail.com>
2026-06-12 11:52:34 +0200

    PORT with 3000 fallback

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,11 +5,13 @@ import logger from "./logger.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
 async function main() {
   await connectDB();
 
-  app.listen(3000, () => {
-    logger.info("Server running on http://localhost:3000");
+  app.listen(PORT, () => {
+    logger.info(`Server running on http://localhost:${PORT}`);
   });
 }
 
